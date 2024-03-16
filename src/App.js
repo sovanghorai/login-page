0add13d6fba33344f20d42ea2256af4f852c7c94
sovanghorai <sovanghorai6@gmail.com>
2024-03-16 13:59:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { useState } from 'react';
+import './App.css';
+import { TfiKey } from "react-icons/tfi";
 function App() {
+  const [state,setState] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+   <>
+   
+   <div className="w-screen h-screen bg-gray-300 flex justify-center items-center">
+     <div className="container  bg-white  "> 
+      <div className="box ">
+        <div className="text-box ">
+            <div className="logo"><TfiKey /></div>
+            <h5>Password required</h5>
+            <p className='text'>You need a Password to access the file</p>
+            <p className='Untitled'>Untitled</p>
+        </div>
+        <div className="input-box">
+            <input type="text" name="" id="" placeholder='Enter password'/>
+            <span>{state}</span>
+            <button type="submit" onClick={()=>setState("Please enter a valid password")}>Continue</button>
+        </div>
+      </div>
+     </div>
+   </div>
+   </>
   );
 }
 
